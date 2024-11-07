9a44f6485de2b3566095774b60363f46ff5d4856
Steven Yao <stevenyao2016@gmail.com>
2024-11-07 16:59:19 -0500

    added direction for predator object

--- PA3_Fall2024/ModelLinkage.py
+++ PA3_Fall2024/ModelLinkage.py
@@ -66,7 +66,7 @@ class Predator(Component, EnvironmentObject):
     rotation_speed = []
     translation_speed = None
     speed = None
-    direction = 1
+    direction = Point([0,0,1])
 
     def __init__(self, parent, position, shaderProg, display_obj=None):
         super(Predator, self).__init__(position)
@@ -277,9 +277,16 @@ class Predator(Component, EnvironmentObject):
                     else:
                         self.translation_speed.reflect(Point([1,1,1]))
             
-
+        # finding the quaternion needed to get to new target direction
+        target_direction = self.translation_speed.normalize()
+        # u in the quaternion
+        norm_cross_prod = self.direction.cross3d(target_direction).normalize()
+        dot_prod = self.direction.dot(target_direction)
+        angle = numpy.arccos(dot_prod)
+        new_quat = Quaternion(numpy.cos(angle/2), norm_cross_prod[0] * numpy.sin(angle/2), norm_cross_prod[1] * numpy.sin(angle/2), norm_cross_prod[2] * numpy.sin(angle/2))
+        main_component.setQuaternion(new_quat)
         # Update position by translation speed
-        new_pos = pos + (self.translation_speed * self.direction)
+        new_pos = pos + (self.translation_speed)
         main_component.setCurrentPosition(new_pos)
 class Prey(Component, EnvironmentObject):
     """
